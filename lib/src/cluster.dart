c4256f078206bc3edfb23ec659b26ac6f910ffb9
ddurzo <dario.durzo@brunellocucinelli.it>
2022-01-21 10:11:19 +0100

    - add max-dist algorithm

--- lib/src/cluster.dart
+++ lib/src/cluster.dart
@@ -5,12 +5,26 @@ class Cluster<T extends ClusterItem> {
   final LatLng location;
   final Iterable<T> items;
 
-  Cluster(this.items)
-      : this.location = LatLng(
+  Cluster(this.items, this.location);
+
+  Cluster.fromItems(Iterable<T> items)
+      : this.items = items,
+        this.location = LatLng(
             items.fold<double>(0.0, (p, c) => p + c.location.latitude) /
                 items.length,
             items.fold<double>(0.0, (p, c) => p + c.location.longitude) /
                 items.length);
+
+  //location becomes weighted avarage lat lon
+  Cluster.fromClusters(Cluster<T> cluster1, Cluster<T> cluster2)
+      : this.items = cluster1.items.toSet()..addAll(cluster2.items.toSet()),
+        this.location = LatLng(
+            (cluster1.location.latitude * cluster1.count +
+                cluster2.location.latitude * cluster2.count) /
+                (cluster1.count + cluster2.count),
+            (cluster1.location.longitude * cluster1.count +
+                cluster2.location.longitude * cluster2.count) /
+                (cluster1.count + cluster2.count));
 
   /// Get number of clustered items
   int get count => items.length;
@@ -30,4 +44,7 @@ class Cluster<T extends ClusterItem> {
   String toString() {
     return 'Cluster of $count $T (${location.latitude}, ${location.longitude})';
   }
+
+  bool operator ==(o) => o is Cluster && items == o.items;
+  int get hashCode => items.hashCode;
 }
