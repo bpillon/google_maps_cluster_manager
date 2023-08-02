fb6d1d87b7dfb16e5c3338fc12c759fd924ac811
Eike Bartels <eike.t.bartels@gmail.com>
2023-08-02 10:07:59 +0200

    Updated to >=3.0.5 <4.0.0'

--- lib/src/cluster_item.dart
+++ lib/src/cluster_item.dart
@@ -1,7 +1,7 @@
 import 'package:google_maps_cluster_manager/google_maps_cluster_manager.dart';
 import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
 
-abstract class ClusterItem {
+mixin ClusterItem {
   LatLng get location;
 
   String? _geohash;
