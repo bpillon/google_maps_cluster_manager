b655c3ff9651c5bea00af755a3649313bb5f00cc
Vincenzo De Petris <vincenzo.depetris@eng.it>
2024-04-17 10:36:32 +0200

    fix: use selective imports

--- lib/src/cluster_manager.dart
+++ lib/src/cluster_manager.dart
@@ -5,7 +5,15 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_cluster_manager/google_maps_cluster_manager.dart';
 import 'package:google_maps_cluster_manager/src/max_dist_clustering.dart';
-import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
+import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart'
+    show
+        GoogleMapsFlutterPlatform,
+        CameraPosition,
+        BitmapDescriptor,
+        Marker,
+        MarkerId,
+        LatLng,
+        LatLngBounds;
 
 enum ClusterAlgorithm { GEOHASH, MAX_DIST }
 
